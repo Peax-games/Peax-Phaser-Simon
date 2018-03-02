5cae0c5c4f31505aad3726843723f3950b45ccab
Christopher Fox <cfox1290@gmail.com>
2018-03-02 13:03:00 -0700

    probably broken

--- src/Game/states/boot.js
+++ src/Game/states/boot.js
@@ -1,5 +1,11 @@
 export default function bootState(){
     return{
+        preload: function () {
+
+            this.load.image('preloaderBackground', 'assets/preloadbck.png');
+            this.load.image('preloaderBar', 'assets/preloadbar.png');
+    
+        },    
         create: function(){
             this.game.physics.startSystem(window.Phaser.Physics.ARCADE);
             this.game.state.start('load');
--- src/Game/states/load.js
+++ src/Game/states/load.js
@@ -1,7 +1,7 @@
-export default function loadState(){
-    return{
-        preload: function(){
-        var loadingLabel = this.game.add.text(80, 150, 'loading...',
+export default function loadState() {
+    return {
+        preload: function () {
+            var loadingLabel = this.game.add.text(80, 150, 'loading...',
                 { font: '30px Courier', fill: '#fff' });
 
             this.game.scale.scaleMode = window.Phaser.ScaleManager.SHOW_ALL;
@@ -10,8 +10,24 @@ export default function loadState(){
             //game.stage.backgroundColor = '#eee';
 
             this.game.load.spritesheet('item', 'img/number-buttons.png', 160, 160);
+
+            this.bck = this.add.sprite(this.world.centerX, this.world.centerY, 'preloaderBackground');
+            this.bck.anchor.setTo(0.5, 0.5);
+            this.bck.scale.setTo(0.5, 0.5);
+            this.preloadBar = this.add.sprite(this.world.centerX, this.world.centerY, 'preloaderBar');
+            this.preloadBar.anchor.setTo(0, 0.5);
+            this.preloadBar.scale.setTo(0.5, 1);
+            this.preloadBar.x = this.world.centerX - this.preloadBar.width / 2;
+
+
+            this.load.setPreloadSprite(this.preloadBar);
+
+            this.load.atlas('spriteset', 'assets/spriteset.png', 'assets/spriteset.jsona');
+            this.load.image('tweet', 'assets/twit.png');
+            this.load.audio('sfx', ['assets/sfx.mp3', 'assets/sfx.ogg', 'assets/sfx.wav', 'assets/sfx.m4a']);
+
         },
-        create: function(){
+        create: function () {
             this.game.state.start('play')
         }
     }
