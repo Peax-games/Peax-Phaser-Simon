ee13be926427f3e44d6bf4b7953e3612fbc89471
Christopher Fox <cfox1290@gmail.com>
2018-03-01 17:49:13 -0700

    I think it works?

--- src/Game/states/load.js
+++ src/Game/states/load.js
@@ -1,5 +1,18 @@
 export default function loadState(){
     return{
-        
+        preload: function(){
+        var loadingLabel = this.game.add.text(80, 150, 'loading...',
+                { font: '30px Courier', fill: '#fff' });
+
+            this.game.scale.scaleMode = window.Phaser.ScaleManager.SHOW_ALL;
+            this.game.scale.pageAlignHorizontally = true;
+            this.game.scale.pageAlignVertically = true;
+            //game.stage.backgroundColor = '#eee';
+
+            this.game.load.spritesheet('item', 'img/number-buttons.png', 160, 160);
+        },
+        create: function(){
+            this.game.state.start('play')
+        }
     }
 }
